8c676108e0af0b51aff51a8b5ac5a46f41f94bdb
TusharVaid <tusharvaid30@gmail.com>
2022-03-09 21:38:57 +0530

    Script Change

--- Assets/Scripts/WriteImageData.cs
+++ Assets/Scripts/WriteImageData.cs
@@ -24,8 +24,6 @@ public class WriteImageData : MonoBehaviour
 
     private void Start()
     {
-        path = data.path;
-        OpenFile();
     }
 
     private void Update()
@@ -43,40 +41,45 @@ public class WriteImageData : MonoBehaviour
 
     private void SetupData()
     {
-        WriteStringLine("{");
-        WriteStringLine("     \"categories\":" + " [");
-        for (var i = 0; i < panels.Length; i++)
-        {
-            WriteStringLine("       {");
-            WriteStringLine("         \"supercategory\": \"" + panels[i].name + "\",");
-            WriteStringLine("         \"id\":" + i + ",");
-            WriteStringLine("         \"name\": \"" + panels[i].name + "\"");
-            WriteStringLine(i == panels.Length - 1 ? "     }" : "      },");
-        }
-        WriteStringLine("      ],");
-        WriteStringLine("     \"images\":" + " [");
         for (var i = 0; i <= data.numberOfFrames - 1; i++)
         {
+            path = data.path + "_" + (i + 1) + ".png.json";
+            OpenFile();
+
+
+            WriteStringLine("{");
+            WriteStringLine("     \"categories\":" + " [");
+            for (var a = 0; a < panels.Length; a++)
+            {
+                WriteStringLine("       {");
+                WriteStringLine("         \"supercategory\": \"" + panels[a].name + "\",");
+                WriteStringLine("         \"id\":" + a + ",");
+                WriteStringLine("         \"name\": \"" + panels[a].name + "\"");
+                WriteStringLine(a == panels.Length - 1 ? "     }" : "      },");
+            }
+
+            WriteStringLine("      ],");
+            WriteStringLine("     \"images\":" + " [");
             WriteStringLine("       {");
             WriteStringLine("         \"height\":  1080,");
             WriteStringLine("         \"width\":  1920,");
             WriteStringLine("         \"id\": " + i + ",");
             WriteStringLine("         \"file_name\": \"" + carName + "_" + (i + 1) + ".png\"");
-            WriteStringLine(i == data.numberOfFrames - 1 ? "     }" : "      },");
-        }
-        WriteStringLine("     ],");
-        WriteStringLine("     \"annotations\":" + " [");
-        for (var i = 0; i <= data.numberOfFrames - 1; i++)
-        {
-            
+            WriteStringLine("     }");
+
+
+            WriteStringLine("     ],");
+            WriteStringLine("     \"annotations\":" + " [");
+
+
             for (var x = 0; x < panels.Length; x++)
             {
                 if (panels[x].GetComponent<FramesAndCoords>().data.ContainsKey(i))
                 {
                     WriteStringLine("        {");
                     WriteStringLine("           \"iscrowd\": 0,");
-                    WriteStringLine("           \"image_id\":" + i +",");
-            
+                    WriteStringLine("           \"image_id\":" + i + ",");
+
                     WriteStringLine("           \"bbox\": [");
 
                     var bboxX = new List<float>();
@@ -87,28 +90,33 @@ public class WriteImageData : MonoBehaviour
                         bboxX.Add(panels[x].GetComponent<FramesAndCoords>().data[i][z].x);
                         bboxY.Add(panels[x].GetComponent<FramesAndCoords>().data[i][z].y);
                     }
-                    
+
                     WriteStringLine("           " + Mathf.Min(bboxX.ToArray()) + ",");
                     WriteStringLine("           " + Mathf.Min(bboxY.ToArray()) + ",");
-                    WriteStringLine("           " + (Mathf.Max(bboxX.ToArray()) - Mathf.Min(bboxX.ToArray())) + ",");
+                    WriteStringLine("           " + (Mathf.Max(bboxX.ToArray()) - Mathf.Min(bboxX.ToArray())) +
+                                    ",");
                     WriteStringLine("           " + (Mathf.Max(bboxY.ToArray()) - Mathf.Min(bboxY.ToArray())));
                     WriteStringLine("           ],");
-                    
+
                     WriteStringLine("          \"segmentation\" : [");
                     WriteStringLine("           [");
                     for (var j = 0; j < panels[x].GetComponent<FramesAndCoords>().data[i].Length; j++)
                     {
-                        WriteString("                       [" + panels[x].GetComponent<FramesAndCoords>().data[i][j].x + ", " + 
+                        WriteString("                       " +
+                                    panels[x].GetComponent<FramesAndCoords>().data[i][j].x + ", " +
                                     panels[x].GetComponent<FramesAndCoords>().data[i][j].y);
 
-                        WriteStringLine(j == panels[x].GetComponent<FramesAndCoords>().data[i].Length - 1 ? "]" : "],");
+                        WriteStringLine(
+                            j == panels[x].GetComponent<FramesAndCoords>().data[i].Length - 1 ? "" : ",");
                     }
+
                     WriteStringLine("            ]");
                     WriteStringLine("        ],");
-                    WriteStringLine("           \"category_id\":" + x + ",");
+                    WriteStringLine("           \"category_name\":" + "\"" + panels[x].name + "\" ,");
                     WriteStringLine("           \"id\":" + annID + ",");
-                    WriteStringLine("           \"area\":" + (Mathf.Max(bboxX.ToArray()) - Mathf.Min(bboxX.ToArray())) *
-                        (Mathf.Max(bboxY.ToArray()) - Mathf.Min(bboxY.ToArray())));
+                    WriteStringLine("           \"area\":" +
+                                    (Mathf.Max(bboxX.ToArray()) - Mathf.Min(bboxX.ToArray())) *
+                                    (Mathf.Max(bboxY.ToArray()) - Mathf.Min(bboxY.ToArray())));
                     if (x == panels.Length - 1 && i == data.numberOfFrames - 1)
                         WriteStringLine("     }");
                     else
@@ -118,6 +126,7 @@ public class WriteImageData : MonoBehaviour
                 }
             }
         }
+
 
         WriteStringLine("]}");
 
